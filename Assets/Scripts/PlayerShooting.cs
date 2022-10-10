0eb5b4dda8159ca17a6a398d94ade6b72f44c0f3
Daniel Szajkowski <nolfinisko@gmail.com>
2022-10-10 17:37:14 +0200

    Boost Power Up implemented
    Implemented boost for player shooting speed, usable every 40 seconds for 5 seconds

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public float timeToShoot = 0.4f;
+    public static float timeToShoot = 2f;
     public GameObject bulletPrefab;
     
     private IEnumerator _coroutine;
+    private IEnumerator _boostCoroutine = BoostActivate();
+    private static bool _isBoostReady = true;
     
     void Start()
     {
@@ -23,5 +25,20 @@ public class PlayerShooting : MonoBehaviour
             Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             yield return new WaitForSeconds(timeToShoot);
         }
+    }
+
+    public void BoostTapped()
+    {
+        if(_isBoostReady) StartCoroutine(_boostCoroutine);
+    }
+
+    static IEnumerator BoostActivate()
+    {
+        _isBoostReady = false;
+        timeToShoot = 1;
+        yield return new WaitForSeconds(5);
+        timeToShoot = 2;
+        yield return new WaitForSeconds(40);
+        _isBoostReady = true;
     }
 }
